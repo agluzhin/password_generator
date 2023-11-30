9219665d57117602426657c6d777d049477efbbd
agluzhin <agluzhin@gmail.com>
2023-11-30 14:05:25 +0300

    Update main.py
    Change the syntax solution of this case

--- main.py
+++ main.py
@@ -1,25 +1,17 @@
-# Import "random" module
+# Import "random" and "string" module
 import random
+import string
 
 # Creating variable in which user can pass the password's length.
 password_length = int(input())
 
-# Creating a storage for the all elements in future password.
-new_password = []
-
 # Creating a storage of all possible elements.
-elements = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q",
-    "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+elements = string.digits + string.ascii_letters + string.punctuation
 
-# Creating a cycle in which new password get random element from element's storage.	
-for i in range(0, password_length):
-	random_element = random.choice(elements)
-	new_password.append(random_element)
-	i += 1
+# Creating a cycle in which new password get random element from element's storage.
+new_password = ''.join(
+    random.choice(elements)
+    for _ in range(password_length)
+)
 
-# Creating variable in which list is converted to a string for further output.
-result = ''.join(map(str, new_password))
-print(result)
-	
-
-
+print(new_password)
